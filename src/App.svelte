a089a8526ef1490fdd2fe98ec0880f848a343650
Alex Ward <alxwrd@googlemail.com>
2020-03-07 12:21:38 +0000

    fix routing on github pages

--- src/App.svelte
+++ src/App.svelte
@@ -4,4 +4,13 @@
   import Index from "./pages/Index.svelte";
 </script>
 
-<Index />
+{#if location.hostname !== "localhost"}
+    <base href="foodtime/">
+{/if}
+
+
+<Router>
+  <Route exact component={Index} />
+  <Route exact path="#" component={Index} />
+</Router>
+
